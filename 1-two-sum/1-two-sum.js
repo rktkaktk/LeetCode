4c72d0c8a846e4e17a20348924d7e94584b7df31
rktkaktk <rktkaktk@gmail.com>
2022-01-13 03:15:29 +0900

    Time: 155 ms (28.16%), Space: 40.3 MB (52.93%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,20 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // nums의 number를 키로, index값을 value로 갖는 obj 만들기
+    // 반복문을 돌면서 target - n 값으로 조건에 맞는값 존재여부 체크
+    let hash = {}
+    
     for (let i = 0; i < nums.length; i++) {
-      for (let j = i + 1; j < nums.length; j++) {
-        if (target === nums[i] + nums[j]) {
-            return [i, j];
+      const n = nums[i];
+        
+        if (hash[target - n] !== undefined) {
+            return [hash[target - n], i];
         }
-      }   
+        hash[n] = i;
     }
     
-    // let completemet = 0;
-    // for (let i = 0; i < nums.length; i++) {
-    //     completemet = target - nums[i];
-    //   if (completemet === nums[i]) {
-    //     return [i, j];
-    //   }
-    // }
+    return [];
     
 };
