666aa9b70a841a494d903051f09c73f1fa56a02f
rktkaktk <rktkaktk@gmail.com>
2022-02-10 08:53:57 +0900

    Time: 102 ms (68.27%), Space: 44.2 MB (34.70%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,6 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  return JSON.stringify(s.split('').sort()) === JSON.stringify(t.split('').sort());
+  function createNewMap(str) {
+    let map = new Map();
     
+    for (let word of str) {
+      if (map.has(word)) {
+        map.set(word, map.get(word) + 1);
+      } else {
+        map.set(word, 1);  
+      }
+    }
+    
+    return map;
+  }
+  
+  let mapS = createNewMap(s);
+  let mapT = createNewMap(t);
+  
+  if (mapS.size !== mapT.size) {
+    return false;  
+  }
+  
+  return [...mapS].every(([key, val], idx) => {
+    return val === mapT.get(key);
+  });
 };
