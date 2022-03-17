a56aed659582f17520a49cb32b1c1d9ebcba3866
rktkaktk <rktkaktk@gmail.com>
2022-03-18 08:37:26 +0900

    Time: 80 ms (79.10%), Space: 43.7 MB (37.58%) - LeetHub

--- 231-power-of-two/231-power-of-two.js
+++ 231-power-of-two/231-power-of-two.js
@@ -2,14 +2,23 @@
  * @param {number} n
  * @return {boolean}
  */
+
+// 8 = 4, 2, 
+// 9 = 4.5
 var isPowerOfTwo = function(n) {
-  if (n < 2) {
-    if (n === 1 || n === 2) {
+  if (n === 1) {
+    return true;
+  }
+  
+  if (n <= 2) {
+    if (n === 2) {
       return true;    
     } else {
       return false;
     }
-  } else {
+  }
+  
+  if (n > 2) {
     return isPowerOfTwo(n/2);
   }
 };
